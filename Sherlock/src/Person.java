390326a1153993ffbaba4587cd7874b4ea252f9b
yelizavieta <lisavetkara@gmail.com>
2023-04-07 16:15:23 +0300

    add new project Person

---
+++ Sherlock/src/Person.java
@@ -0,0 +1,6 @@
+public class Person {
+    public String personInfo(String name, String surname, String city, String phone) {
+        String info = "Зателефонувати громадянину " + name + " " + surname + " із міста " + city + " можна за номером " + phone;
+        return info;
+    }
+}
